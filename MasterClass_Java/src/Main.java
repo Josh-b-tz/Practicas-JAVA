a4c2f3ade49532e308b3d77a2bfbe241e7a067a1
Josue Batz <josuebatz0.1@gmail.com>
2025-12-18 15:39:17 -0600

    borrando archivos viejos

--- MasterClass_Java/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        bank bobsAccount = new bank();
-        bobsAccount.withdrawFunds(100);
-
-    }
-}
